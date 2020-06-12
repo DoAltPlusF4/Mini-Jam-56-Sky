8030dee5e338c73df89fdc3965ae47d36ef5ccb8
Lenny P Critchley <doaltplusf4@gmail.com>
2020-06-12 18:26:39 +0100

    Added load_animation.

---
+++ engine/load_animation.py
@@ -0,0 +1,25 @@
+import pyglet
+
+def load_animation(image, data):
+    sprite_sheet = pyglet.image.ImageGrid(
+        image,
+        len(data["animations"]),
+        data["max_length"]
+    )
+    animations = {}
+    for a in range(len(data["animations"])):
+        animation_data = data["animations"][a]
+        frames = []
+        for i in range(animation_data["length"]):
+            frame_length = data["animations"][a]["frame_length"]
+            frames.append(
+                sprite_sheet[(a, i)]
+            )
+        animations[
+            data["animations"][a]["alias"]
+        ] = pyglet.image.Animation.from_image_sequence(
+            frames,
+            frame_length,
+            loop=data["animations"][a]["loop"]
+        )
+    return animations
